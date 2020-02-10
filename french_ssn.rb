86edc37df9685b701b48a3333941c9805068f1a0
Prima Aulia Gusta <primAulia@gmail.com>
2020-02-10 18:52:24 +0100

    latest

--- french_ssn.rb
+++ french_ssn.rb
@@ -1,10 +1,14 @@
 require 'date'
 require 'yaml'
 
+def checksum?(ssn)
+  (97 - ssn.delete(' ')[0..12].to_i) % 97
+end
+
 def french_ssn_info(ssn)
   pattern = /(?<gender>[1|2])\s?(?<yob>\d{2})\s?(?<mob>\d{2})\s?(?<dept>\d{2})\s?\d{3}\s?\d{3}\s?\d{2}/
 
-  return "The number is invalid" if ssn == ""
+  return "The number is invalid" unless ssn.match?(pattern)
 
   # decompose the string via regex DONE
   # use regex method match to pull each info out
@@ -30,5 +34,14 @@ def french_ssn_info(ssn)
   # puts dept  
   # p YAML.load_file('data/french_departments.yml')
 
-  puts "a #{gender}, born in #{mob}, #{yob} in #{dept}."
+  if checksum?(ssn)
+    return "a #{gender}, born in #{mob}, #{yob} in #{dept}."
+  else
+    return "The number is invalid"
+  end
+
 end
+
+
+
+
